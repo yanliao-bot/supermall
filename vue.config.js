0133692df57b1700ca7d8316cebc04f404f1f0f8
unknown <yanliao@hz-soft.com>
2021-04-08 12:08:04 +0800

    添加文件

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports={
+configurewebpack: {
+resolve:{
+     //别名
+     alias:{
+       'assets':'@/assets',
+       'common':'@/common',
+       'comments':'@/comments',
+       'views':'@/views',
+       'network':'@/network',
+     }
+
+    
+}
+
+}
+
+
+}
